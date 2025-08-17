247e3a2dbf3915470401adb9e1c6acff995fefc0
Marcel Santos <vldasul@yahoo.com.br>
2025-08-17 01:14:46 -0300

    declarando variaveis

--- OlaMundo/aula1.c
+++ OlaMundo/aula1.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
    int main() {
-    printf("Olá, Mundo!\n");
+
+    printf("\nOlá, Mundo!\n");
 
     return 0;
 
---
+++ OlaMundo/declararvariavel.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+    int main(){
+        int idade;
+        int quantidade;
+        float altura;
+        double peso;
+        char letra;
+        char nome[10];
+
+        //Não pode iniciar nomes de váriaveis com numero
+        //Letras maiuscula e minuscular 
+        //Não pode usar palavras reservadas
+        //Usar nomes representativos no programa 
+
+}
