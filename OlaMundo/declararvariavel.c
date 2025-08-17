d1eaf7d35974a3e01b7a3836e44b98e96757d914
Marcel Santos <vldasul@yahoo.com.br>
2025-08-17 01:22:10 -0300

    atribuições

--- OlaMundo/declararvariavel.c
+++ OlaMundo/declararvariavel.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
 
     int main(){
-        int idade;
-        int quantidade;
-        float altura;
-        double peso;
-        char letra;
-        char nome[10];
+        int idade = 25;
+        int quantidade = 4;
+        float altura = 1.79;
+        double peso = 125.50;
+        char letra = 'A';
+        char nome[10] = "Rodrigo";
+
+        //Se colocar novos valores depois da inicialização, elas atualizam
+
+        idade = 34;
+        quantidade = 9;
 
         //Não pode iniciar nomes de váriaveis com numero
         //Letras maiuscula e minuscular 
